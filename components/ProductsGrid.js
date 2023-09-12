46c02759a8d7ce790dfa3d3941b9ed3c9613b1ca
Thịnh Nhất Lê <68993699+thinhnhatle2002@users.noreply.github.com>
2023-09-12 19:43:12 +0700

    check

---
+++ components/ProductsGrid.js
@@ -0,0 +1,30 @@
+import ProductBox from "./ProductBox";
+
+const { styled } = require("styled-components");
+
+
+
+const StyledProductsGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    @media screen and (min-width: 768px){
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    
+`;
+
+
+
+
+export default function ProductsGrid({products}) {
+    return (
+        <StyledProductsGrid>
+            {products?.length > 0 && products.map(product =>(
+                <div key={product._id}>
+                    <ProductBox key={product._id} {...product}></ProductBox>
+                </div>
+            ))}
+        </StyledProductsGrid>
+    );
+}
