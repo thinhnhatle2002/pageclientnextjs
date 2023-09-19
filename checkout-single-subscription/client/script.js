0206652b56909510ae5d1f72fb756ceb06f1fb2c
Thịnh Nhất Lê <68993699+thinhnhatle2002@users.noreply.github.com>
2023-09-19 15:16:00 +0700

    check

--- checkout-single-subscription/client/script.js
+++
@@ -1,9 +0,0 @@
-/* Fetch prices and update the form */
-fetch("/config")
-  .then(r => r.json())
-  .then(({basicPrice, proPrice}) => {
-    const basicPriceInput = document.querySelector('#basicPrice');
-    basicPriceInput.value = basicPrice;
-    const proPriceInput = document.querySelector('#proPrice');
-    proPriceInput.value = proPrice;
-  })
--- pages/api/cart.js
+++
@@ -1,8 +0,0 @@
-import { mongooseConnection } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
-
-export default async function handle(req,res){
-    await mongooseConnection();
-    const ids = req.body.ids;
-    res.json(await Product.find({_id:ids}));
-}
